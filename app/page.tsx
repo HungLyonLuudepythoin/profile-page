08163147ed60caea1904c467912ed06cc539a1c7
unknown <vinhhungluu402@gmail.com>
2024-10-15 19:11:42 +0700

    Add favicon and images

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,19 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Fav_manga from "./components/Fav_manga";
+import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
+       <Head>
+        <title>My portfolio</title>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+          <link rel="manifest" href="/site.webmanifest"/>
+      </Head>
       <Navbar/>
       <Hero/>
       <Fav_manga/>
